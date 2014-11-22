b81a16901ab27ebb9b4c540105edc30a1aba2a44
Ken Lynch <kenlynch@computer.org>
2014-11-22 09:23:19 -0500

    get all congress votes`
    Retrieve all votes through history

--- vs2012/console/getCongressVotes/getCongressVotes/Program.cs
+++ vs2012/console/getCongressVotes/getCongressVotes/Program.cs
@@ -12,19 +12,174 @@ namespace getCongressVotes
     {
         static void Main(string[] args)
         {
-            WebClient webClient = new WebClient();
             String uri;
             String filePath;
-            for (int nVote = 1; nVote <= 515; nVote++)
+            int nVote;
+            bool isDone = false;
+            int nYear;
+            
+            
+            int nCongress = 1;
+            isDone = false;
+            nVote = 1;
+            nYear = 1;
+            for (nCongress = 1; nCongress <= 76; nCongress++)
             {
-                uri = String.Format("https://www.govtrack.us/data/congress/113/votes/2014/h{0}/data.xml", nVote.ToString());
-                filePath = String.Format(@"D:\Coursera\SNA\h{0}.xml", nVote.ToString());
-                Console.WriteLine(uri + " - " + filePath);
-                try
+                isDone = false;
+                nVote = 1;
+                nYear = 1;
+                int needYear = 2;
+
+                switch (nCongress)
                 {
-                    webClient.DownloadFile(uri, filePath);
+                    case 1:
+                    case 5:
+                    case 11:
+                    case 13:
+                    case 25:
+                    case 34:
+                    case 37:
+                    case 40:
+                    case 41:
+                    case 42:
+                    case 45:
+                    case 46:
+                    case 53:
+                    case 55:
+                    case 58:
+                    case 61:
+                    case 62:
+                    case 63:
+                    case 64:
+                    case 65:
+                    case 66:
+                    case 71:
+                    case 75:
+                    case 76:
+                        needYear = 3;
+                        break;
+                    case 67:
+                        needYear = 4;
+                        break;
+                    default:
+                        needYear = 2;
+                        break;
                 }
-                catch { }
+
+                while(!isDone)
+                {
+                    filePath = String.Format(@"D:\Coursera\SNA\{0}\h{1}.xml", nCongress.ToString(),nVote.ToString());
+                    if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                    uri = String.Format("https://www.govtrack.us/data/congress/{0}/votes/{1}/h{2}/data.xml", nCongress.ToString(), nYear.ToString(), nVote.ToString());
+                    try
+                    {
+                        getFile(uri, filePath);
+                        nVote++;
+                    }
+                    catch
+                    {
+                        if (nYear >= needYear)
+                           isDone= true;
+                        else
+                            nYear++;
+                    }
+                }
+                isDone = false;
+                nVote = 1;
+                nYear = 1;
+
+                while (!isDone)
+                {
+                    filePath = String.Format(@"D:\Coursera\SNA\{0}\s{1}.xml", nCongress.ToString(),nVote.ToString());
+                    if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+                    uri = String.Format("https://www.govtrack.us/data/congress/{0}/votes/{1}/s{2}/data.xml", nCongress.ToString(),nYear.ToString(), nVote.ToString());
+                    try
+                    {
+                        getFile(uri, filePath);
+                        nVote++;
+                    }
+                    catch
+                    {
+                        if (nYear >= needYear)
+                            isDone = true;
+                        else
+                            nYear++;
+                    }
+                }
+            }
+        }
+
+            for (nCongress = 77; nCongress <= 113; nCongress++)
+            {
+                isDone = false;
+                nVote = 1;
+                if (nCongress == 77) nVote = 2;
+                nYear = (nCongress - 77) * 2 + 1941;
+                int needYear = nYear + 1;
+
+                while(!isDone)
+                {
+                    filePath = String.Format(@"D:\Coursera\SNA\{0}\h{1}.xml", nCongress.ToString(),nVote.ToString());
+                    if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                    uri = String.Format("https://www.govtrack.us/data/congress/{0}/votes/{1}/h{2}/data.xml", nCongress.ToString(), nYear.ToString(), nVote.ToString());
+                    try
+                    {
+                        getFile(uri, filePath);
+                        nVote++;
+                    }
+                    catch
+                    {
+                        if (nYear >= needYear)
+                           isDone= true;
+                        else
+                            nYear++;
+                    }
+                }
+                isDone = false;
+                nVote = 1;
+                nYear = (nCongress - 77) * 2 + 1941;
+                needYear = nYear + 1;
+
+                while (!isDone)
+                {
+                    filePath = String.Format(@"D:\Coursera\SNA\{0}\s{1}.xml", nCongress.ToString(),nVote.ToString());
+                    if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+                    uri = String.Format("https://www.govtrack.us/data/congress/{0}/votes/{1}/s{2}/data.xml", nCongress.ToString(),nYear.ToString(), nVote.ToString());
+                    try
+                    {
+                        getFile(uri, filePath);
+                        nVote++;
+                    }
+                    catch
+                    {
+                        if (nYear >= needYear)
+                            isDone = true;
+                        else
+                            nYear++;
+                    }
+                }
+            }
+        }
+
+        
+        
+        static void getFile(String uri, String path)
+        {
+            WebClient webClient = new WebClient();
+            Console.WriteLine(uri + " - " + path);
+            try
+            {
+                webClient.DownloadFile(uri, path);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
     }
